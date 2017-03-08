71b2d11b68d74849c721c0d0cd45a4c657d03cac
hasauino <oh91@windowslive.com>
2017-03-08 19:39:36 +0400

    update

--- MrYellow_Arduino_library/MrYellow/MrYellow.h
+++ MrYellow_Arduino_library/MrYellow/MrYellow.h
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Encoder.h>
+
 #define TWI_FREQ 37390L
 #define DEVICE_ADDRESS 0x05
 #define M1_MODE 0x44
@@ -17,6 +19,12 @@
 #define HEAD_CW 49
 #define HEAD_CCW 50
 #define FACE 102
+
+#define RIGHT_MOTOR2_BLACK 26
+#define RIGHT_MOTOR2_WHITE 28
+#define RIGHT_MOTOR2_PWM 13
+#define RIGHT_MOTOR2_YELLOW 19
+#define RIGHT_MOTOR2_BLUE 22
 
 //right motor is motor 1
 //left motor is motor 2
@@ -142,6 +150,27 @@ switch (b[i]) {
 
 
 
+void right_up_2(int power,Encoder & enc){
+analogWrite(RIGHT_MOTOR2_PWM,power);
+digitalWrite(RIGHT_MOTOR2_BLACK,LOW);
+digitalWrite(RIGHT_MOTOR2_WHITE,HIGH);
+boolean cond=true;
+long t0=millis();
+while((enc.read()*-0.5)<120 && cond){  cond=(millis()-t0)<3000;  }
+analogWrite(RIGHT_MOTOR2_PWM,0);
+enc.write(120*-2);
+}
 
+
+void right_down_2(int power,Encoder & enc){
+analogWrite(RIGHT_MOTOR2_PWM,power);
+digitalWrite(RIGHT_MOTOR2_BLACK,HIGH);
+digitalWrite(RIGHT_MOTOR2_WHITE,LOW);
+boolean cond=true;
+long t0=millis();
+while((enc.read()*-0.5)>0 && cond){  cond=(millis()-t0)<3000;  }
+analogWrite(RIGHT_MOTOR2_PWM,0);
+enc.write(0);
+}
 
 
